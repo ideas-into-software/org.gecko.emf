08d5fb2f6acd6757c9d51f23117353485022dd9f
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2022-02-11 15:07:26 +0100

    fixup license headers
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.emf.osgi/src/org/gecko/emf/osgi/UriMapProvider.java
+++ org.gecko.emf.osgi/src/org/gecko/emf/osgi/UriMapProvider.java
@@ -1,13 +1,15 @@
-/*******************************************************************************
- * Copyright (c) 2012 Bryan Hunt.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
+/**
+ * Copyright (c) 2012 - 2022 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made available under the terms of the 
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
  * Contributors:
- *    Bryan Hunt - initial API and implementation
- *******************************************************************************/
+ *      Bryan Hunt - initial API
+ *      Data In Motion - implementation
+ */
 package org.gecko.emf.osgi;
 
 import java.util.Map;
--- org.gecko.emf.osgi/src/org/gecko/emf/osgi/components/ResourceSetCacheComponent.java
+++ org.gecko.emf.osgi/src/org/gecko/emf/osgi/components/ResourceSetCacheComponent.java
@@ -1,14 +1,15 @@
-/*******************************************************************************
- * Copyright (c) 2012 Bryan Hunt.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
+/**
+ * Copyright (c) 2012 - 2022 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made available under the terms of the 
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
  * Contributors:
- *    Bryan Hunt - initial API and implementation
- *    Data In Motion Consulting GmbH
- *******************************************************************************/
+ *      Bryan Hunt - initial API
+ *      Data In Motion - implementation
+ */
 package org.gecko.emf.osgi.components;
 
 import java.util.concurrent.atomic.AtomicReference;
