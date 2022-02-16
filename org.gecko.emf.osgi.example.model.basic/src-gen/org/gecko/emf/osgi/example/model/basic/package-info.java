f3dedecd0558b7661058876c62731815af78876f
Juergen Albert <j.albert@data-in-motion.biz>
2022-02-16 17:23:15 +0100

    untracked generated code
    Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

--- org.gecko.emf.osgi.example.model.basic/src-gen/org/gecko/emf/osgi/example/model/basic/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * This program and the accompanying materials are made available under the terms of the
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
- */
-@org.osgi.annotation.versioning.Version("1.0.0")
-@org.osgi.annotation.bundle.Export
-package org.gecko.emf.osgi.example.model.basic;
