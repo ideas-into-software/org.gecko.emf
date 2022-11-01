475557c9f84c0f951484ad86f8b4c9688adfd252
Juergen Albert <j.albert@data-in-motion.biz>
2022-11-01 14:42:38 +0100

    removed unused projects and updated licence header
    Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

--- org.gecko.emf.osgi.example.model.manual/src-gen/org/gecko/emf/osgi/example/model/manual/util/ManualResourceFactoryImpl.java
+++ org.gecko.emf.osgi.example.model.manual/src-gen/org/gecko/emf/osgi/example/model/manual/util/ManualResourceFactoryImpl.java
@@ -1,7 +1,15 @@
-/**
- * This program and the accompanying materials are made available under the terms of the
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
+/*
+ * Copyright (c) 2012 - 2022 Data In Motion and others.
+ * All rights reserved. 
+ *  
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *  
+ * Contributors:
+ *       Data In Motion - initial API and implementation
  */
 package org.gecko.emf.osgi.example.model.manual.util;
 
@@ -11,6 +19,15 @@ import org.eclipse.emf.ecore.resource.Resource;
 
 import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
 
+import org.gecko.emf.osgi.annotation.provide.ProvideEMFResourceConfigurator;
+
+import org.gecko.emf.osgi.example.model.manual.ManualPackage;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ServiceScope;
+
 /**
  * <!-- begin-user-doc -->
  * The <b>Resource Factory</b> associated with the package.
@@ -18,6 +35,16 @@ import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
  * @see org.gecko.emf.osgi.example.model.manual.util.ManualResourceImpl
  * @generated
  */
+ @Component( name = ManualPackage.eNAME + "Factory", service = Resource.Factory.class, scope = ServiceScope.SINGLETON,
+ 	reference = @Reference( name = ManualPackage.eNAME + "Package", service = ManualPackage.class, cardinality = ReferenceCardinality.MANDATORY)
+ )
+ @ProvideEMFResourceConfigurator( name = ManualPackage.eNAME,
+	contentType = { "manual#1.0" }, 
+	fileExtension = {
+	"manual"
+ 	},  
+	version = "1.0"
+)
 public class ManualResourceFactoryImpl extends ResourceFactoryImpl {
 	/**
 	 * Creates an instance of the resource factory.
--- org.gecko.emf.osgi.extender.itest/src/org/gecko/emf/osgi/extender/itest/package-info.java
+++ org.gecko.emf.osgi.extender.itest/src/org/gecko/emf/osgi/extender/itest/package-info.java
@@ -1,17 +1,14 @@
 /**
  * Copyright (c) 2012 - 2022 Data In Motion and others.
  * All rights reserved. 
+ *  
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
  * 
- * This program and the accompanying materials are made available under the terms of the 
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
- * 
+ * SPDX-License-Identifier: EPL-2.0
+ *  
  * Contributors:
- *     Data In Motion - initial API and implementation
- */
-/**
- * 
- * @author mark
- * @since 14.10.2022
+ *       Data In Motion - initial API and implementation
  */
 package org.gecko.emf.osgi.extender.itest;
