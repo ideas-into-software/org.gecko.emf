8a7eb2449516966a6fed9f630bc6b94012f4f6af
Juergen Albert <j.albert@data-in-motion.biz>
2023-01-27 14:27:29 +0100

    adds support for emf provide capability
    Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

---
+++ org.gecko.emf.osgi/src/org/gecko/emf/osgi/annotation/provide/EPackage.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2012 - 2022 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
+ */
+package org.gecko.emf.osgi.annotation.provide;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.osgi.annotation.bundle.Capability;
+
+/**
+ * Marker annotation that the bundle has the capability to provide a certain
+ * model and allows the ecore editor to find this in a repository.
+ * 
+ * @author Juergen Albert
+ * @since 9 Feb 2018
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.TYPE, ElementType.PACKAGE })
+@Capability(namespace = "org.eclipse.emf.ecore.generated_package",
+	attribute = {
+		"class=\"${#value}\"", //
+		"uri=${#uri}",
+		"genModel=${#genModel}"
+	})
+public @interface EPackage {
+
+	String uri();
+
+	Class<?> value() default Target.class;
+
+	String genModel();
+}
