2a7522d9e4e126c7b7f43ac1668b2b9df26b8f91
Juergen Albert <j.albert@data-in-motion.biz>
2024-01-24 19:18:16 +0100

    Fixed initialize by loading support in codegen
    Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

--- org.gecko.emf.osgi.codegen/src/org/gecko/emf/osgi/codegen/adapter/GeckoGenPackageGeneratorAdapter.java
+++ org.gecko.emf.osgi.codegen/src/org/gecko/emf/osgi/codegen/adapter/GeckoGenPackageGeneratorAdapter.java
@@ -13,7 +13,10 @@
  */
 package org.gecko.emf.osgi.codegen.adapter;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -26,8 +29,27 @@ import org.eclipse.emf.codegen.ecore.genmodel.GenResourceKind;
 import org.eclipse.emf.codegen.ecore.genmodel.generator.GenPackageGeneratorAdapter;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.Monitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EModelElement;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.resource.impl.BinaryResourceImpl.EObjectOutputStream;
+import org.eclipse.emf.ecore.resource.impl.BinaryResourceImpl.BinaryIO.Version;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.XMLResource.URIHandler;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.URIHandlerImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.gecko.emf.osgi.codegen.GeckoEmfGenerator;
+import org.gecko.emf.osgi.codegen.ResourceUriHandler;
 
 /**
  * EMF codegen generator adapter that is responsible to generate the OSGi
@@ -100,7 +122,7 @@ public class GeckoGenPackageGeneratorAdapter extends GenPackageGeneratorAdapter 
 
 		super.generateSchema(genPackage, monitor);
 		super.generatePackagePublication(genPackage, monitor);
-		super.generatePackageSerialization(genPackage, monitor);
+		generatePackageSerialization(genPackage, monitor);
 		super.generateXMLProcessorClass(genPackage, monitor);
 		super.generateResourceClass(genPackage, monitor);
 		generatePackageInterface(genPackage, monitor);
@@ -122,6 +144,238 @@ public class GeckoGenPackageGeneratorAdapter extends GenPackageGeneratorAdapter 
 		}
 
 		return Diagnostic.OK_INSTANCE;
+	}
+
+	private static class ExtendedEcoreResourceFactoryImpl extends EcoreResourceFactoryImpl {
+		@Override
+		public Resource createResource(URI uri) {
+			XMLResource result = new XMIResourceImpl(uri) {
+				@Override
+				protected boolean useIDs() {
+					return eObjectToIDMap != null || idToEObjectMap != null;
+				}
+
+				@Override
+				protected EObjectOutputStream createEObjectOutputStream(OutputStream outputStream, Map<?, ?> options,
+						Version version, final URIHandler uriHandler) throws IOException {
+					return new EObjectOutputStream(outputStream, options, version) {
+						@Override
+						public void writeURI(URI uri, String fragment) throws IOException {
+							// We need to ensure that the callback being used for redirecting to the nsURI
+							// is invoked also for binary serialization, which relies on the fragment being
+							// present.
+							URI deresolvedURI = deresolve(uri.appendFragment(fragment));
+							super.writeURI(deresolvedURI, deresolvedURI.fragment());
+						}
+
+						@Override
+						protected URI deresolve(URI uri) {
+							return uriHandler == null ? super.deresolve(uri) : uriHandler.deresolve(uri);
+						}
+					};
+				}
+			};
+			result.setEncoding("UTF-8");
+
+			if ("genmodel".equals(uri.fileExtension())) {
+				result.getDefaultLoadOptions().put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
+			}
+
+			result.getDefaultSaveOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
+			result.getDefaultSaveOptions().put(XMLResource.OPTION_LINE_WIDTH, 80);
+			result.getDefaultSaveOptions().put(XMLResource.OPTION_URI_HANDLER,
+					new URIHandlerImpl.PlatformSchemeAware());
+			return result;
+		}
+	}
+
+	protected void generatePackageSerialization(GenPackage genPackage, Monitor monitor) {
+		if (genPackage.hasClassifiers() && genPackage.isLoadingInitialization()) {
+			monitor = createMonitor(monitor, 1);
+
+			try {
+				monitor.beginTask("Serializing Ecore because initialize by loading is set to true", 2);
+
+				final GenModel genModel = genPackage.getGenModel();
+				String targetPathName = genModel.getModelDirectory() + "/"
+						+ genPackage.getClassPackageName().replace('.', '/') + "/"
+						+ genPackage.getSerializedPackageFilename();
+				message = CodeGenEcorePlugin.INSTANCE.getString("_UI_GeneratingPackageSerialization_message",
+						new Object[] { targetPathName });
+				monitor.subTask(message);
+
+				URI targetFile = toURI(targetPathName);
+				ensureContainerExists(targetFile.trimSegments(1), createMonitor(monitor, 1));
+
+				final ResourceSet originalSet = genModel.eResource().getResourceSet();
+				EPackage originalPackage = genPackage.getEcorePackage();
+				final String originalPackageNsURI = originalPackage.getNsURI();
+
+				ResourceSet outputSet = new ResourceSetImpl();
+				GeckoEmfGenerator.info("Setting Uri Handler: " + originalSet.getURIConverter().getURIHandlers().get(0));
+				outputSet.getURIConverter().getURIHandlers().add(0,
+						originalSet.getURIConverter().getURIHandlers().get(0));
+				outputSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
+						.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new ExtendedEcoreResourceFactoryImpl());
+				URI targetURI = toPlatformResourceURI(targetFile);
+				Resource outputResource = outputSet.createResource(targetURI);
+
+				// Copy the package, excluding unwanted annotations.
+				//
+				EcoreUtil.Copier copier = new EcoreUtil.Copier() {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					protected void copyContainment(EReference reference, EObject object, EObject copyEObject) {
+						if (reference == EcorePackage.Literals.EMODEL_ELEMENT__EANNOTATIONS) {
+							List<EAnnotation> result = ((EModelElement) copyEObject).getEAnnotations();
+							result.clear();
+
+							for (EAnnotation eAnnotation : ((EModelElement) object).getEAnnotations()) {
+								if (!genModel.isSuppressedAnnotation(eAnnotation.getSource())) {
+									result.add((EAnnotation) copy(eAnnotation));
+								}
+							}
+							return;
+						}
+						super.copyContainment(reference, object, copyEObject);
+					}
+				};
+				EPackage outputPackage = (EPackage) copier.copy(originalPackage);
+				copier.copyReferences();
+				outputResource.getContents().add(outputPackage);
+				collapseEmptyPackages(outputPackage);
+
+				// This URI handler redirects cross-document references to correct
+				// namespace-based values.
+				//
+				XMLResource.URIHandler uriHandler = new URIHandlerImpl.PlatformSchemeAware() {
+					private EPackage getContainingPackage(EObject object) {
+						while (object != null) {
+							if (object instanceof EPackage) {
+								return (EPackage) object;
+							}
+							object = object.eContainer();
+						}
+						return null;
+					}
+
+					private String getRelativeFragmentPath(Resource resource, EObject base, String path) {
+						String basePath = resource.getURIFragment(base);
+						if (basePath != null && path.startsWith(basePath)) {
+							int i = basePath.length();
+							if (path.length() == i) {
+								return "";
+							} else if (path.charAt(i) == '/') {
+								return path.substring(i);
+							}
+						}
+						return null;
+					}
+
+					private EPackage getNonEmptySuperPackage(EPackage ePackage) {
+						EPackage result = ePackage.getESuperPackage();
+						while (result != null && result.getEClassifiers().isEmpty()) {
+							result = result.getESuperPackage();
+						}
+						return result;
+					}
+
+					private URI redirect(URI uri) {
+						if (uri != null && !uri.isCurrentDocumentReference() && uri.hasFragment()) {
+							URI base = uri.trimFragment();
+							String fragment = uri.fragment();
+							Resource resource = originalSet.getResource(base, false);
+							if (resource != null) {
+								EObject object = resource.getEObject(fragment);
+								if (object != null) {
+									EPackage ePackage = getContainingPackage(object);
+									if (ePackage != null) {
+										String relativePath = getRelativeFragmentPath(resource, ePackage, fragment);
+										if (relativePath != null) {
+											StringBuilder path = new StringBuilder();
+											EPackage superPackage = getNonEmptySuperPackage(ePackage);
+											while (superPackage != null) {
+												path.insert(0, '/');
+												path.insert(1, ePackage.getName());
+												ePackage = superPackage;
+												superPackage = getNonEmptySuperPackage(ePackage);
+											}
+											path.insert(0, '/');
+											path.append(relativePath);
+
+											// With Xcore generation, the previously serialized resource might have been
+											// loaded into the resource set.
+											// So if the package nsURI is the same as the nsURI of the package we're
+											// generating,
+											// and if the resource URI is the one to which we are serializing,
+											// redirect to the base URI so that it's subsequently deresolved to a
+											// document relative URI.
+											String nsURI = ePackage.getNsURI();
+											URI redirectedURI = originalPackageNsURI.equals(nsURI)
+													&& resource.getURI().equals(baseURI) ? baseURI
+															: URI.createURI(nsURI);
+											return redirectedURI.appendFragment(path.toString());
+										}
+									}
+								}
+							}
+						}
+						return uri;
+					}
+
+					@Override
+					public URI deresolve(URI uri) {
+						return super.deresolve(redirect(uri));
+					}
+
+					@Override
+					public URI resolve(URI uri) {
+						return super.resolve(redirect(uri));
+					}
+
+					@Override
+					public void setBaseURI(URI uri) {
+						super.setBaseURI(redirect(uri));
+					}
+				};
+				Map<Object, Object> options = new HashMap<Object, Object>();
+				options.put(XMLResource.OPTION_URI_HANDLER, uriHandler);
+				options.put(Resource.OPTION_SAVE_ONLY_IF_CHANGED, Resource.OPTION_SAVE_ONLY_IF_CHANGED_MEMORY_BUFFER);
+				if ("ebin".equals(outputResource.getURI().fileExtension())) {
+					options.put(XMLResource.OPTION_BINARY, Boolean.TRUE);
+				} else {
+					options.put(Resource.OPTION_LINE_DELIMITER, Resource.OPTION_LINE_DELIMITER_UNSPECIFIED);
+				}
+
+				try {
+					GeckoEmfGenerator.info("saving ecore to " + outputResource.getURI().toString());
+					outputResource.save(options);
+				} catch (IOException exception) {
+					// DMS handle this well.
+					GeckoEmfGenerator.error("Could not serialize ecore to " + outputResource.getURI().toString(), exception);
+				}
+			} finally {
+				GeckoEmfGenerator.info("Finished Serialization of ecore");
+				monitor.done();
+			}
+		} else {
+			monitor.worked(1);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.emf.codegen.ecore.generator.AbstractGeneratorAdapter#
+	 * toPlatformResourceURI(org.eclipse.emf.common.util.URI)
+	 */
+	@Override
+	protected URI toPlatformResourceURI(URI uri) {
+		GeckoEmfGenerator.info("toPlatformResourceURI: " + uri.toString());
+		URI result = super.toPlatformResourceURI(uri);
+		GeckoEmfGenerator.info("toPlatformResourceURI esult: " + uri.toString());
+		return result;
 	}
 
 	@Override
@@ -231,10 +485,10 @@ public class GeckoGenPackageGeneratorAdapter extends GenPackageGeneratorAdapter 
 			message = String.format("Generating package interface info for package '%s'",
 					genPackage.getQualifiedPackageInterfaceName());
 			monitor.subTask(message);
-			
+
 			@SuppressWarnings("unchecked")
 			Map<String, Object> props = (Map<String, Object>) getAdapterFactory().getGenerator().getOptions().data[0];
-			
+
 			generateJava(genModel.getModelDirectory(), genPackage.getReflectionPackageName(),
 					genPackage.getPackageInterfaceName(), getJETEmitter(getJETEmitterDescriptors(), PACKAGE_CLASS),
 					new Object[] { new Object[] { genPackage, Boolean.TRUE, Boolean.FALSE, props } },
